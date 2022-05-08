303d71468e8ec567375f258c2270515985358dbd
Samson Weldegiorgis <S.WELDEGIORGIS7347@edmail.edcc.edu>
2022-05-08 09:44:45 -0700

    Basic Navigations

--- src/test/java/com/cydeo/test/day1_selenium_intro/Basic_Navigations.java
+++ src/test/java/com/cydeo/test/day1_selenium_intro/Basic_Navigations.java
@@ -20,10 +20,32 @@ public class Basic_Navigations {
         // 4. using back i.e. navigating back from the web page
         driver.navigate().back();
 
-        // 5. stoppkng applications, we are givnig time to applns. to see steps
-        Thread.sleep(15000);
-        driver.quit();
+        // 5. stopping applications, we are givnig time to applns. to see steps
+//        Thread.sleep(3000);
+//        driver.quit();
 
+        // 6. Navigate to forward in the web Page
+          driver.navigate().forward();
+
+        Thread.sleep(3000);
+
+        // 7. navigate refreshing the web page
+        driver.navigate().refresh();
+
+        // 8. Get the current title......of etsy
+        System.out.println("Etsy page driver.getTitle() = " + driver.getTitle());
+
+        // getting url of the current page.....of etsy
+        System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
+
+        // 7. navigate the web page ...of Tesla
+        driver.get("https://www.tesla.com");
+
+        // 8. Get the current title......of Tesla
+        System.out.println("Tesla page driver.getTitle() = " + driver.getTitle());
+
+        // 9. Getting the url .......of Tesla
+        System.out.println("driver.getCurrentUrl() = " + driver.getCurrentUrl());
 
 
     }
